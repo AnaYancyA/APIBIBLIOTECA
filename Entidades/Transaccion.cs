e831cdd036d15b4ef8d16de739f879f76f68cc68
Ana Yancy Aguilar <Ana Yancy Aguilar@ANAYANCY>
2026-08-12 15:34:03 -0600

    Agregar archivos de proyecto.

---
+++ Entidades/Transaccion.cs
@@ -0,0 +1,13 @@
+﻿namespace APIBIBLIOTECA.Capamodelo
+{
+    public class Transaccion
+    {
+        public int Id { get; set; }
+        public string TipoTransaccion { get; set; } // "Préstamo" o "Devolución"
+        public int IdLibro { get; set; }
+        public Libro Libro { get; set; } // Relación con la clase Libro
+        public int IdUsuario { get; set; }
+        public Usuario Usuario { get; set; } // Relación con la clase Usuario
+        public DateTime Fecha { get; set; }
+    }
+}
